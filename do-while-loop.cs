071fb2be2b77f801bb2044692fe0771c5f49a8ce
parmis delfani <parmis.delfani84@gmail.com>
2024-09-24 01:26:33 +0330

    Add files via upload

---
+++ do-while-loop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do
+            //{
+            //    // code block to be executed
+            //}
+            //while (condition);
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 5);
+            Console.ReadKey();
+
+        }
+    }
+}
