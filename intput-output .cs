ae4c1c9f52c9c3f8aee9d5b61f339dca8c89c03e
parmis delfani <parmis.delfani84@gmail.com>
2024-09-24 01:29:35 +0330

    Add files via upload

---
+++ intput-output .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //intput/ output 
+            string name;
+            Console.WriteLine("hey... what is your name?");
+            name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Nice to meet you {0}",name);
+            Console.WriteLine("Well iam 19 years old are, how are you");
+            Console.ReadLine();
+            Console.WriteLine("Ohhhhhhhhhh nice");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ miniproject-if-else.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            string numberainput = Console.ReadLine();
+            int numbera = Convert.ToInt32(numberainput);
+
+            Console.WriteLine("Enter the second number");
+            string numberbinput = Console.ReadLine();
+            int numberb = Convert.ToInt32(numberbinput);
+
+            int answer = numbera * numberb;
+
+
+            Console.WriteLine("value of" + numbera + "x" + numberb + ":");
+            string answerinput = Console.ReadLine();
+            int actualanswer = Convert.ToInt32(answerinput);
+
+            if(answer == actualanswer)
+            {
+                Console.WriteLine("well done");
+            }
+            else
+            {
+                Console.WriteLine("close but it was wrong");
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
