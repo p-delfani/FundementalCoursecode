4f53662e64cd2fdcca8d3992c4e2454bcab9e590
parmis delfani <parmis.delfani84@gmail.com>
2024-09-24 02:02:19 +0330

    Add files via upload

---
+++ conditional-operators.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //conditional operators
+            // if your age is 10  result is valid 
+            // if your age is -10 result is invaild
+            int age = 10;
+
+            string result = age >= 0 ? "valid" : "invalid";
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
