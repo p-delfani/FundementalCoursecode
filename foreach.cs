6e011b7157c86cbda1104c5211ed38b85b4680d1
parmis delfani <parmis.delfani84@gmail.com>
2024-09-24 01:28:28 +0330

    Add files via upload

---
+++ foreach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach (type variableName in arrayName)
+            //{
+            //    // code block to be executed
+            //}
+
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
