33aff3fd4e787aa24b3fec0e85463ad3f44ed7f7
parmis delfani <parmis.delfani84@gmail.com>
2024-09-24 01:25:36 +0330

    Add files via upload

---
+++ reminder,cs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reminder
+
+            int firstnnum = 10;
+            int secondnum = 3;
+
+            Console.WriteLine(firstnnum / secondnum); //the result is 3 beacuse this is a integer
+            Console.WriteLine(firstnnum % secondnum); // the result is 1
+
+            Console.ReadKey();
+        }
+
+    }
+}
