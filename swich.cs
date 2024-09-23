9835c77571de07661084264a017a9b0a13b23434
parmis delfani <parmis.delfani84@gmail.com>
2024-09-24 01:30:11 +0330

    Add files via upload

---
+++ swich.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch
+            //switch (expression)
+            //{
+            //    case x:
+            //        // code block
+            //        break;
+            //    case y:
+            //        // code block
+            //        break;
+            //    default:
+            //        // code block
+            //        break;
+            //}
+
+
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ while-loop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (condition)
+            //{
+            //    // code block to be executed
+            //}
+
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
