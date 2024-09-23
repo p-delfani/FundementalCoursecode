1b21ec3da84761899ae620f3e872793a6cba4d11
parmis delfani <parmis.delfani84@gmail.com>
2024-09-24 01:26:50 +0330

    Add files via upload

---
+++ Exercise odd-even checker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise odd/even checker without(if)
+
+            int num1 = 10;
+            int num2 = 2;
+            int reminder = num1 % num2;
+            Console.WriteLine(reminder); //0
+            // now beacuse the result is 0 so 10 is a odd number
+
+            int num3 = 11;
+            int num4 = 2;
+            int reminder2 = num3 % num4;
+            Console.WriteLine(reminder2); //1
+            // now beacuse the result is 1 so 10 is a oven number
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise string user data.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise string user data
+
+            //way1
+            //string name = "parmis"; //parmis
+            //string number = "09195565656"; //09195565656
+            //int age = 19; //19
+
+            //way2
+            var name = "parmis"; //parmis
+            var number = "09195565656"; //09195565656
+            var age = 19; //19
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            Console.ReadKey();
+
+            // so at the result is not any differnce between way 1 or way 2 in result
+        }
+    }
+}
