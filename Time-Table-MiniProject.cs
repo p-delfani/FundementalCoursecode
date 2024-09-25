1268326e67d4d704f2f15ce7ba86302f8ce3349b
parmis delfani <parmis.delfani84@gmail.com>
2024-09-25 18:39:59 +0330

    Add files via upload

---
+++ Time-Table-MiniProject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Time table 
+            Console.WriteLine("Enter your number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0} × {1} = {2}", i, number, i * number);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TryParse-MiniProject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp56
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool success = true;
+
+            while (success)
+            {
+                Console.Write("Enter a number:");
+                string numinput = Console.ReadLine();
+
+                if (int.TryParse(numinput, out int num))
+                {
+                    success = false;
+                    Console.WriteLine(num);
+
+                }
+                else
+                {
+                    Console.WriteLine("failed to convert");
+                }
+                Console.ReadKey();
+
+            }
+            
+        }
+    }
+}
