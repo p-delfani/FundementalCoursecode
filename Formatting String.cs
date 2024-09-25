3cf120e1c370bbcdabf8705a3054d36cf027d1a2
parmis delfani <parmis.delfani84@gmail.com>
2024-09-25 19:38:39 +0330

    Add files via upload

---
+++ Formatting String.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Formatting String
+
+            //string name = "parmis";
+            //int age = 19;
+            //Console.WriteLine("Name: "+name);
+            //Console.WriteLine("age: "+age);
+            //Console.WriteLine();
+            //Console.WriteLine("Name: " + name +"\nage: "+age);
+            //Console.ReadKey();
+
+            Console.WriteLine("Enter your name");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your Name is {0} \nYour age is {1}", name,age);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ String InterPolation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String InterPolation $
+
+            string name = "parmis";
+            int age = 19;
+
+            Console.WriteLine($"your name is {name} your age is {age}");
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
