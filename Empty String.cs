98573de031c4d99ea455933927aec27c2215b08d
parmis delfani <parmis.delfani84@gmail.com>
2024-09-25 19:57:46 +0330

    Add files via upload

---
+++ Empty String.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Empty String
+
+            Console.WriteLine("Enter your name PLease ...");
+            string name = Convert.ToString(Console.ReadLine());
+            
+
+            if(name != "")
+            {
+                Console.WriteLine("Hello and welcome dear {0}",name);
+            }
+            else
+            {
+                Console.WriteLine("Your name is Empty");
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
