68c7d8f9cc7e90e11fd52b1373918a2223f26eae
parmis delfani <parmis.delfani84@gmail.com>
2024-09-25 18:00:35 +0330

    Add files via upload

---
+++ Implicit Casting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Casting
+
+            //int myInt = 9;
+            //double myDouble = myInt;       // Automatic casting: int to double
+
+            //Console.WriteLine(myInt);      // Outputs 9
+            //Console.WriteLine(myDouble);   // Outputs 9
+
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+
+            Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tryparse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Try Parse
+            Console.WriteLine("Enter a numebr");
+            string numinput = Console.ReadLine(); //10h
+            int num = 0;
+            int.TryParse(numinput, out num);
+
+            Console.WriteLine(num); //0
+
+            Console.ReadKey();
+
+        }
+    }
+}
