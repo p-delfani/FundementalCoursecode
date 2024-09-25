91a903b44432f8b82133bc287376a6cc853f36af
parmis delfani <parmis.delfani84@gmail.com>
2024-09-25 19:14:07 +0330

    Add files via upload

---
+++ Verbatim String.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verbatim String
+            // \n \t \" \\= \
+            //\n = new line
+            // \t = tab
+            // \\ = print one \
+
+
+            string speech = "He Said \"some things\" ";
+            string path = "C:\\Users\\Parmis\\Final-Projects"; 
+            Console.WriteLine(speech); //He Said "some things"
+            Console.WriteLine(path);  //C:\Users\Parmis\Final-Projects
+
+            path = @"C:\Users\Parmis\Final-Projects"; 
+
+            Console.WriteLine(path); //C:\Users\Parmis\Final-Projects
+            Console.ReadKey();
+
+
+        }
+    }
+}
