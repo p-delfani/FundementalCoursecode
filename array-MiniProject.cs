2747023491c3a649cdc00c08f1e2faa206100f5c
parmis delfani <parmis.delfani84@gmail.com>
2024-09-29 22:15:30 +0330

    Add files via upload

---
+++ array-MiniProject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] names = new string[10, 4];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write($"نام فرد {i * 4 + j + 1}: ");
+                    names[i, j] = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("\nاسامی وارد شده:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(names[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
